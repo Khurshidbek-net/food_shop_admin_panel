b8f78e400e89040b1064135421a15aea5bf72c9f
Khurshidbek <xurshidbekoktamaliyev651@gmail.com>
2025-01-24 10:33:39 +0500

    product added

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ const express = require('express');
 const config = require('config');
 const exHbs = require("express-handlebars");
 const viewRouter = require("./routes/view.routes");
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const mainRoute = require("./routes/index.routes");
-=======
-const MainRout = require("./routes/index")
->>>>>>> 6d5657b68558733f4963c303b8b8538cc2613eab
 
 
 const port = config.get("port");
@@ -30,23 +26,16 @@ app.set("views", "./views")
 app.use(express.static("public"));
 
 
-<<<<<<< HEAD
 app.use("/", viewRouter); 
 app.use("/api", mainRoute);
-=======
-app.use("/", viewRouter);  
-app.use("/api", MainRout); 
->>>>>>> 6d5657b68558733f4963c303b8b8538cc2613eab
+
 
 
 async function start() {
   try {
-<<<<<<< HEAD
     await mongoose.connect(config.get("db_url"));
     console.log("Db Connected");
-=======
     
->>>>>>> 6d5657b68558733f4963c303b8b8538cc2613eab
     app.listen(port, () => {
       console.log(`Server running: http://localhost:${port}`)
     });
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,7 +3,10 @@ const router = require("express").Router();
 
 const  categoryRoute = require("./category.routes");
 const adminRoute = require("./admin.routes");
+const ProductRoute = require("./products.route");
 
+
+router.use("/product", ProductRoute);
 router.use("/category", categoryRoute);
 router.use("/admin", adminRoute);
 
